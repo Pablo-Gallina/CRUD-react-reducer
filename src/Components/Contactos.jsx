0b273e2447297360dc33c00cb404fb6acca028ec
Pablo Gallina <pablogallinag@gmail.com>
2021-11-25 21:58:34 -0600

    Guardar los datos en el localStorage

--- src/Components/Contactos.jsx
+++ src/Components/Contactos.jsx
@@ -1,15 +1,22 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import { contactosReducer } from '../reducers/contactosReducer'
 import Modal from './Modal/Modal'
 import Form from './Form/Form'
 import Table from './Table/Table'
 
-
-const contactos = []
+const init = ()=>{
+    const contactos = localStorage.getItem("contactos");
+    return contactos ? JSON.parse(contactos) : []
+}
 
 const Contactos = () => {
     //state = estado inicial, dispatch = funcion que va a disparar el cambio de estado, como un setState
-    const [state, dispatch] = useReducer(contactosReducer, contactos) //Recibe como parametros, funcion reducer (funcion que creamos), el estado inicial (lista de contactos)
+    const [state, dispatch] = useReducer(contactosReducer, [], init) //Recibe como parametros, funcion reducer (funcion que creamos), el estado inicial (lista de contactos)
+
+    useEffect(() => {
+        localStorage.setItem("contactos", JSON.stringify(state))
+    }, [state])
+    
     return (
         <>  
             <Modal body={<Form dispatch={dispatch} />} />
